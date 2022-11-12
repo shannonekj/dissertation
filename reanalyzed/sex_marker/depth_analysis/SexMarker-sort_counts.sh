67be856814b902eec3306b0d7e77d09a07e636ed
Shannon EK Joslin <sejoslin@ucdavis.edu>
2022-11-12 14:02:56 -0800

    change back to gz ...doesn't matter

--- reanalyzed/sex_marker/depth_analysis/SexMarker-sort_counts.sh
+++ reanalyzed/sex_marker/depth_analysis/SexMarker-sort_counts.sh
@@ -9,4 +9,4 @@ grep -v $'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0' ${infil
 sort -nrk 9 ${infile}.no0 >> ${infile}.no0.sort
 sed -i '$ d' ${infile}.no0.sort
 rm ${infile}.no0
-lzma ${infile}
+gzip ${infile}
