7bd15fefbd260bf4de6ad374cdc0f436a481e0b6
Shannon EK Joslin <sejoslin@ucdavis.edu>
2022-11-12 06:27:38 -0800

    initial commit

---
+++ reanalyzed/sex_marker/depth_analysis/SexMarker-sort_counts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+infile=$1
+
+sed -n 1p ${infile}  > ${infile}.no0.sort
+grep -v $'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0' ${infile} > ${infile}.no0
+sort -nrk 9 ${infile}.no0 >> ${infile}.no0.sort
+sed -i '$ d' ${infile}.no0.sort
+rm ${infile}.no0
+gzip ${infile}
