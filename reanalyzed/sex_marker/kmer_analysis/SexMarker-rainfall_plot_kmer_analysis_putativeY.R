5f4ecd9897cf5c071eaacf7a8e947f7c22a627e1
Shannon EK Joslin <sejoslin@ucdavis.edu>
2023-01-06 06:46:24 -0800

    get rid of trying to facet plot the karyoplots

--- reanalyzed/sex_marker/kmer_analysis/SexMarker-rainfall_plot_kmer_analysis_putativeY.R
+++ reanalyzed/sex_marker/kmer_analysis/SexMarker-rainfall_plot_kmer_analysis_putativeY.R
@@ -62,8 +62,8 @@ scf9_region <- put_Y %>% filter(chr == scf9, na.rm=TRUE)
 min(scf9_region$end_pos)
 max(scf9_region$start_pos)
 #### get bams
-scf9_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/male.scaffold_9.merged.subset.bam"
-scf9_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/female.scaffold_9.merged.subset.bam"
+scf9_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/male.scaffold_9.merged.subset.bam"
+scf9_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/female.scaffold_9.merged.subset.bam"
 #### define zoom area
 #zoom.region <- toGRanges(data.frame(genome=male_genome, chromosomes="scaffold_9", beg_reg, end_reg))
 scf9_kp <- plotKaryotype(genome=male_genome, chromosomes="scaffold_9", zoom="scaffold_9:6110000-6420000")
@@ -82,8 +82,8 @@ lg09 <- "lg09"
 lg09_region <- put_Y %>% filter(chr == lg09, na.rm=TRUE)
 min(lg09_region$start_pos)
 max(lg09_region$end_pos)
-lg09_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/female.lg09.merged.subset.bam"
-lg09_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/male.lg09.merged.subset.bam"
+lg09_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/female.lg09.merged.subset.bam"
+lg09_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/male.lg09.merged.subset.bam"
 lg09_kp <- plotKaryotype(genome=male_genome, chromosomes="lg09", zoom="lg09:21000000-21100000")
 kpPlotRegions(lg09_kp, data=pY_regs)
 kpAddBaseNumbers(lg09_kp, tick.dist=10000, tick.len=10, tick.col="red", cex=1, 
@@ -99,8 +99,8 @@ kpAddLabels(lg09_kp, "Female", r0=0.5, r1=1, label.margin = 0.05)
 lg17_region <- put_Y %>% filter(chr == "lg17", na.rm=TRUE)
 min(lg17_region$start_pos)
 max(lg17_region$end_pos)
-lg17_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/female.lg17.merged.subset.bam"
-lg17_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/male.lg17.merged.subset.bam"
+lg17_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/female.lg17.merged.subset.bam"
+lg17_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/male.lg17.merged.subset.bam"
 lg17_kp <- plotKaryotype(genome=male_genome, chromosomes="lg17", zoom="lg17:8100000-8200000")
 kpPlotRegions(lg17_kp, data=pY_regs)
 kpAddBaseNumbers(lg17_kp, tick.dist=10000, tick.len=10, tick.col="red", cex=1, 
@@ -116,8 +116,8 @@ kpAddLabels(lg17_kp, "Female", r0=0.5, r1=1, label.margin = 0.05)
 lg21_region <- put_Y %>% filter(chr == "lg21", na.rm=TRUE)
 min(lg21_region$start_pos)
 max(lg21_region$end_pos)
-lg21_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/female.lg21.merged.subset.bam"
-lg21_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/male.lg21.merged.subset.bam"
+lg21_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/female.lg21.merged.subset.bam"
+lg21_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/male.lg21.merged.subset.bam"
 lg21_kp <- plotKaryotype(genome=male_genome, chromosomes="lg21", zoom="lg21:9815000-9860000")
 kpPlotRegions(lg21_kp, data=pY_regs)
 kpAddBaseNumbers(lg21_kp, tick.dist=10000, tick.len=10, tick.col="red", cex=1, 
@@ -133,8 +133,8 @@ kpAddLabels(lg21_kp, "Female", r0=0.5, r1=1, label.margin = 0.05)
 lg23_region <- put_Y %>% filter(chr == "lg23", na.rm=TRUE)
 min(lg23_region$start_pos)
 max(lg23_region$end_pos)
-lg23_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/female.lg23.merged.subset.bam"
-lg23_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/male.lg23.merged.subset.bam"
+lg23_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/female.lg23.merged.subset.bam"
+lg23_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/male.lg23.merged.subset.bam"
 lg23_kp <- plotKaryotype(genome=male_genome, chromosomes="lg23", zoom="lg23:10550000-10620000")
 kpPlotRegions(lg23_kp, data=pY_regs)
 kpAddBaseNumbers(lg23_kp, tick.dist=10000, tick.len=10, tick.col="red", cex=1, 
@@ -150,8 +150,8 @@ kpAddLabels(lg23_kp, "Female", r0=0.5, r1=1, label.margin = 0.05)
 scf190_region <- put_Y %>% filter(chr == "scaffold_190", na.rm=TRUE)
 min(scf190_region$start_pos)
 max(scf190_region$end_pos)
-scf190_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/female.scaffold_190.merged.subset.bam"
-scf190_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/male.scaffold_190.merged.subset.bam"
+scf190_female <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/female.scaffold_190.merged.subset.bam"
+scf190_male <- "/Users/miocene/Desktop/git_repos/dissertation/reanalyzed/sex_marker/kmer_analysis/merged_bams_maleRef/male.scaffold_190.merged.subset.bam"
 scf190_kp <- plotKaryotype(genome=male_genome, chromosomes="scaffold_190", zoom="scaffold_190:80000-100000")
 kpPlotRegions(scf190_kp, data=pY_regs)
 kpAddBaseNumbers(scf190_kp, tick.dist=10000, tick.len=10, tick.col="red", cex=1, 
@@ -162,3 +162,7 @@ kpAddLabels(scf190_kp, "Male", r0=0, r1=0.5, label.margin = 0.05)
 kpPlotBAMCoverage(scf190_kp, data=scf190_female, col="#9666FF", border=NA, r0=0.5, r1=1, ymax=600)
 kpAxis(scf190_kp, r0=0.5, r1=1, ymax=600)
 kpAddLabels(scf190_kp, "Female", r0=0.5, r1=1, label.margin = 0.05)
+
+
+
+
